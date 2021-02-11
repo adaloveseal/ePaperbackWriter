daffb2499bb86e76e8f212ab9078f2507ebfaa25
Ada <ada@nowhere>
2021-02-11 20:58:44 +0100

    Fix programmer & add blinkenlight demo

--- rom/romprog/romprog.ino
+++ rom/romprog/romprog.ino
@@ -1,14 +1,23 @@
+/*
+ * Implemented on Arduino Mega
+ * Addresses A0 to A15 are mapped to analog pins A0 to A15 (PORTF and PORTK)
+ * Data is PORTL
+ * /WE is port 36, /OE is 37 (control is PORTC)
+ */
+
 #define WRITE 0b01
 #define READ  0b10
-#define DATA  PORTF
-#define DATA_IN  PINF
-#define DATA_DIR DDRF
-#define ADDR  PORTB
-#define CTRL  PORTH
+#define DATA  PORTL
+#define DATA_IN  PINL
+#define DATA_DIR DDRL
+#define ADDR_LOW PORTF
+#define ADDR_HI  PORTK
+#define CTRL  PORTC
 
-void write(byte addr, byte value) {
+void write(uint16_t addr, byte value) {
 	DATA_DIR = 0xFF;
-	ADDR = addr;
+	ADDR_LOW = addr & 0xFF;
+	ADDR_HI = addr >> 8;
 	DATA = value;
 	delay(1);
 	CTRL = WRITE;
@@ -17,71 +26,115 @@ void write(byte addr, byte value) {
 	delay(1);
 }
 
-void progbyte(byte addr, byte value) {
-	write(0b11010, 0xAA); // 0x5555
-	write(0b01010, 0x55); // 0x2AAA
-	write(0b11010, 0xA0); // 0x5555
+void progbyte(uint16_t addr, byte value) {
+	write(0x5555, 0xAA);
+	write(0x2AAA, 0x55);
+	write(0x5555, 0xA0);
 	write(addr, value);
 }
 
-byte read(byte addr) {
+byte read(uint16_t addr) {
 	DATA_DIR = 0;
-	ADDR = addr;
+	ADDR_LOW = addr & 0xFF;
+	ADDR_HI = addr >> 8;
 	CTRL = READ;
 	delay(1);
 	byte res  = DATA_IN;
+	delay(1);
 	CTRL = 0xFF;
 	return res;
 }
 
 void erase() {
-	write(0b11010, 0xAA); // 0x5555
-	write(0b01010, 0x55); // 0x2AAA
-	write(0b11010, 0x80); // 0x5555
-	write(0b11010, 0xAA); // 0x5555
-	write(0b01010, 0x55); // 0x2AAA
-	write(0b11010, 0x10); // 0x5555
+	write(0x5555, 0xAA);
+	write(0x2AAA, 0x55);
+	write(0x5555, 0x80);
+	write(0x5555, 0xAA);
+	write(0x2AAA, 0x55);
+	write(0x5555, 0x10);
 	delay(200);
 }
 
-void program(byte *prog, byte length) {
-	// We can only address 16 bytes, and bytes 12 & 13 are the RESET vector
-	// ROM chip should be hardwired to address 0b101010101010XXXX
-	if (length > 12) length = 12;
+void program(byte *prog, uint16_t length) {
 	erase();
-	for (byte i = 0; i < length; i++) {
-		progbyte(0b10000 + i, prog[i]);
+	for (uint16_t i = 0; i < length; i++) {
+		progbyte(i, prog[i]);
 	}
-	progbyte(0b11100, 0);
-	progbyte(0b11101, 0);
+	Serial.println("Programming done.");
 }
 
 void setup() {
 	Serial.begin(9600);
-	DDRH = 0xFF;
-	DDRB = 0xFF;
-	delay(5000);
-
-	for (byte i = 0; i < 16; i++)
-		Serial.println(read(0b10000 + i));
-
-	Serial.print("Programming... ");
+	DDRF = 0xFF;
+	DDRK = 0xFF;
+	DDRC = 0xFF;
+	delay(1000);
 	
-	byte prog[] = {
-		0xAD,
-		0xEA,
-		0x55,
-		0x4C,
-		0x00,
-		0x00
-	};
-	program(prog, 6);
+	Serial.print("Programming... ");
+	byte *prog = calloc(256, 1);
+	/* 6522 is A0-A3 (zero page). Program reads:
+	 * LDA #FF
+	 * STA $03; DDRA = 0xFF
+	 * LDA #00
+	 * STA $02: DDRB = 0
+	 * here:
+	 * LDA $00; read PORTB
+	 * BEQ PB0
+	 * LDA #55
+	 * BRA there
+	 * PB0:
+	 * LDA #AA
+	 * there:
+	 * STA $01; write to PORTA
+	 * JMP here
+	 * Noise with distinctive patterns
+	 * JMP $88; infinite loop
+	 */
+	// LDA #FF
+	prog[0x80] = 0xA9;
+	prog[0x81] = 0xFF;
+	// STA $03
+	prog[0x82] = 0x85;
+	prog[0x83] = 0x03;
+	// LDA #00
+	prog[0x84] = 0xA9;
+	prog[0x85] = 0x00;
+	// STA $02
+	prog[0x86] = 0x85;
+	prog[0x87] = 0x02;
+	// LDA $00
+	prog[0x88] = 0xA5;
+	prog[0x89] = 0x00;
+	// BEQ
+	prog[0x8A] = 0xF0;
+	prog[0x8B] = 0x04; // ?
+	// LDA #55
+	prog[0x8C] = 0xA9;
+	prog[0x8D] = 0x55;
+	// BRA
+	prog[0x8E] = 0x4C;
+	prog[0x8F] = 0x92; // ?
+	// LDA #AA
+	prog[0x90] = 0xA9; // This is read as the high bytes of the JMP @. Who cares. Save a byte.
+	prog[0x91] = 0xAA;
+	// STA $01
+	prog[0x92] = 0x85;
+	prog[0x93] = 0x01;
+	// Back to the beginning
+	prog[0x94] = 0x4C;
+	prog[0x95] = 0x88;
+	// Jump table
+	prog[0xFA] = 0x80; // 6502 is little-endian
+	prog[0xFC] = 0x80;
+	prog[0xFE] = 0x80;
+	prog[0xFF] = 67;
+	program(prog, 256);
 	delay(100);
 
 	Serial.println("Done!");
 	
-	for (byte i = 0; i < 16; i++)
-		Serial.println(read(0b10000 + i));
+	for (uint16_t i = 0; i < 256; i++)
+		Serial.println(read(i));
 }
 
 void loop() {}
